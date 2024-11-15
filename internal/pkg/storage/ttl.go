9c2cbc628f5ebfe725f9dbdbfda30fe528e00bbf
Ippolid <iliamishin2006@gmail.com>
2024-11-15 15:38:40 +0300

    fix ttl.go

--- internal/pkg/storage/ttl.go
+++ internal/pkg/storage/ttl.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func CleaningSession(r Storage, closeChan chan struct{}, n time.Duration) {
+func CleaningSession(r *Storage, closeChan chan struct{}, n time.Duration) {
 	for {
 		select {
 		case <-closeChan:
@@ -15,12 +15,13 @@ func CleaningSession(r Storage, closeChan chan struct{}, n time.Duration) {
 		}
 	}
 }
-func (r Storage) Clean() {
+func (r *Storage) Clean() {
 	k := rand.IntN(len(r.inner))
 	k = k - k/2
 	z := 0
-	for i, _ := range r.inner {
-		if r.inner[i].exp <= time.Now().UnixMilli() && r.inner[i].exp != 0 {
+	nowtime := time.Now().UnixMilli()
+	for i := range r.inner {
+		if r.inner[i].exp <= nowtime && r.inner[i].exp != 0 {
 			delete(r.inner, i)
 		}
 		if z > k {
